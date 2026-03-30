2d8eccb7318b456b6f69d15c0f8dee7f11ce5b55
Supratim158 <supratim2005k@gmail>
2026-03-30 21:11:43 +0530

    used brevo

--- utils/smtp_function.js
+++ utils/smtp_function.js
@@ -17,7 +17,36 @@ async function sendEmail(userEmail, message) {
                     }
                 ],
                 subject: "ProjexHub Verification Code",
-                htmlContent: `<h1>Your OTP</h1><h2>${message}</h2>`
+                htmlContent: `
+  <div style="font-family: Arial, sans-serif; padding: 20px; color: #333;">
+    
+    <h2 style="margin-bottom: 10px;">ProjexHub Verification</h2>
+    
+    <p style="font-size: 14px;">
+      Your One-Time Password (OTP) is:
+    </p>
+
+    <div style="
+      font-size: 28px;
+      font-weight: bold;
+      color: #2563eb;
+      margin: 15px 0;
+    ">
+      ${message}
+    </div>
+
+    <p style="font-size: 13px; color: #555;">
+      This code is valid for 5 minutes. Please do not share it with anyone.
+    </p>
+
+    <hr style="margin: 20px 0;" />
+
+    <p style="font-size: 12px; color: #888;">
+      If you didn’t request this, you can safely ignore this email.
+    </p>
+
+  </div>
+`
             },
             {
                 headers: {
