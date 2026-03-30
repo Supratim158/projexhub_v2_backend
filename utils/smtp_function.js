88c5910f0a33fb6a9cd8c0bcd4f609dcaaa53c2d
Supratim158 <supratim2005k@gmail>
2026-03-30 17:12:36 +0530

    fixing smtp

--- utils/smtp_function.js
+++ utils/smtp_function.js
@@ -1,6 +1,8 @@
 const nodemailer = require('nodemailer');
 
 async function sendEmail(userEmail, message) {
+    console.log("Sending email to:", userEmail);
+
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
         port: 465,
@@ -11,24 +13,26 @@ async function sendEmail(userEmail, message) {
         }
     });
 
+    try {
+        await transporter.verify();
+        console.log("SMTP server is ready");
+    } catch (err) {
+        console.log("SMTP ERROR:", err);
+    }
+
     const mailOptions = {
         from: process.env.AUTH_EMAIL,
         to: userEmail,
         subject: "ProjexHub Verification Code",
-        html:  `<h1>ProjexHub Verification Code</h1>
-                <p>Your Verification code is:</p>
-                <h2 style= "color: blue;">${message}</h2>
-                <p>Please enter this code on verification page to complete your registration process.</p>
-                <p>If not you, please ignore.</p>`
+        html: `<h1>ProjexHub Verification Code</h1>
+                <h2>${message}</h2>`
     };
 
     try {
         await transporter.sendMail(mailOptions);
-        console.log("Verification code sent ");
-        
+        console.log("Email sent successfully");
     } catch (error) {
-        console.log("Email sending failed with an error", error);
-        
+        console.log("Email sending failed:", error);
     }
 }
 
