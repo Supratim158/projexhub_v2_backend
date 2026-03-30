cfbe7fdfa5ff782ad4da9e2eea134fb400887195
Supratim158 <supratim2005k@gmail>
2026-03-30 17:30:08 +0530

    fixing smtp2

--- utils/smtp_function.js
+++ utils/smtp_function.js
@@ -10,29 +10,35 @@ async function sendEmail(userEmail, message) {
         auth: {
             user: process.env.AUTH_EMAIL,
             pass: process.env.AUTH_PASSWORD,
+        },
+        tls: {
+            rejectUnauthorized: false
         }
     });
 
     try {
+        console.log("Verifying SMTP...");
         await transporter.verify();
-        console.log("SMTP server is ready");
+        console.log("SMTP verified ✅");
     } catch (err) {
-        console.log("SMTP ERROR:", err);
+        console.log("SMTP VERIFY ERROR ❌:", err);
+        throw err;
     }
 
     const mailOptions = {
         from: process.env.AUTH_EMAIL,
         to: userEmail,
         subject: "ProjexHub Verification Code",
-        html: `<h1>ProjexHub Verification Code</h1>
-                <h2>${message}</h2>`
+        html: `<h1>Code: ${message}</h1>`
     };
 
     try {
+        console.log("Sending mail...");
         await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully");
+        console.log("Email sent successfully ✅");
     } catch (error) {
-        console.log("Email sending failed:", error);
+        console.log("SEND MAIL ERROR ❌:", error);
+        throw error;
     }
 }
 
